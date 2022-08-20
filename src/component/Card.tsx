427609d72d3b716ac774c0692c21a843c8ecc087
Kim, Hyeonseo <dodok8@gmail.com>
2022-08-21 04:07:16 +0900

    레이아웃 완성

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,13 +1,16 @@
+import { Lion } from "../assets";
 import "./Card.scss";
 
 export const Card = (data: { name: string; dom: Date; dDay: string }) => {
   return (
     <div className="card">
-      <div className="sprite"></div>
+      <div className="sprite">
+        <Lion size={65} />
+      </div>
       <div className="info">
-        <div className="name">{data.name}</div>
-        <div className="dom">{data.dom.toLocaleDateString()}</div>
-        <div className="dDay">{data.dDay}</div>
+        <div className="name">Name: {data.name}</div>
+        <div className="dom">DOM: {data.dom.toLocaleDateString()}</div>
+        <div className="dDay">D+Day: {data.dDay}</div>
       </div>
     </div>
   );
